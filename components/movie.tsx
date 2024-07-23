8875cf4668033b812b076d0696d347b65a299000
서연 <155455327+seoyeon1123@users.noreply.github.com>
2024-07-23 15:30:25 +0900

    prefetch

--- components/movie.tsx
+++ components/movie.tsx
@@ -19,7 +19,9 @@ export default function Movie({ id, title, poster_path }: IMoveProps) {
     <>
       <div className={styles.movie}>
         <img src={poster_path} alt={title} onClick={onClick} />
-        <Link href={`/movies/${id}`}>{title}</Link>
+        <Link prefetch href={`/movies/${id}`}>
+          {title}
+        </Link>
       </div>
     </>
   );
