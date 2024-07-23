bc9379162369a363b58e188ac3b2330c94846478
서연 <155455327+seoyeon1123@users.noreply.github.com>
2024-07-23 15:14:06 +0900

    feat : .module.css 변경

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
 import { API_URL } from '../app/(home)/page';
+import styles from '../styles/movie-info.module.css';
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   console.log(`Fetching movies : ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
 
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
@@ -12,7 +14,23 @@ export default async function MovieInfo({ id }: { id: string }) {
   const movies = await getMovie(id);
   return (
     <>
-      <h6>{JSON.stringify(movies)}</h6>
+      <div className={styles.container}>
+        <img
+          className={styles.poster}
+          src={movies.poster_path}
+          alt={movies.title}
+        />
+        <div className={styles.info}>
+          <div className={styles.title_container}>
+            <h1 className={styles.title}>{movies.title}</h1>
+            <a href={movies.homepage} target={'_blank'}>
+              🏠 HomePage &rarr;
+            </a>
+          </div>
+          <h3>⭐️ {movies.vote_average.toFixed(1)}</h3>
+          <p>{movies.overview}</p>
+        </div>
+      </div>
     </>
   );
 }
