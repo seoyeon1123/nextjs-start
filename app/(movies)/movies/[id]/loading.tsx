4901b688ad77689c39d31f78b4e12787ce320112
서연 <155455327+seoyeon1123@users.noreply.github.com>
2024-07-26 10:06:11 +0900

    feat : Loading 수정

--- app/(movies)/movies/[id]/loading.tsx
+++ app/(movies)/movies/[id]/loading.tsx
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import styles from '../styles/loading.module.css';
+import styles from '../../../../styles/loading.module.css';
 
 export default function Loading() {
   return (
     <>
       <div className={styles.spinnerContainer}>
-        <FontAwesomeIcon icon={faSpinner} spin size="4x" />
+        <FontAwesomeIcon icon={faSpinner} spin size="3x" />
       </div>
     </>
   );
